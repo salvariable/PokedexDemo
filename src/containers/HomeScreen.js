eef49806c34806d2d3329a7c0eb8421c0b354da0
Salvador <45272829+salvariable@users.noreply.github.com>
2020-09-09 22:58:17 -0500

    Save pokemon names in AsyncStorage

--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -1,23 +1,38 @@
 import React, { useEffect, useState, Fragment } from 'react';
 import { View, Text } from 'react-native';
 import PokemonGrid from '../components/PokemonGrid';
-import { TextInput } from 'react-native-gesture-handler';
 import { Searchbar } from 'react-native-paper';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default ({ navigation }) => {
   const [pokemons, setPokemons] = useState(null);
   const [searchResults, setSearchResults] = useState(null);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
-      .then((response) => response.json())
-      .then((json) => {
-        setPokemons(json.results);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+    const getCacheData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem('POKEMON_CACHE');
+        const names = jsonValue != null ? JSON.parse(jsonValue) : null;
+        return setPokemons(names);
+      } catch (error) {
+        console.log('Error getting data: ', error);
+      }
+    };
+
+    getCacheData();
+
+    if (!pokemons) {
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=1050')
+        .then((response) => response.json())
+        .then(async (json) => {
+          setPokemons(json.results);
+          await AsyncStorage.setItem('POKEMON_NAMES', JSON.stringify(json.results));
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [pokemons]);
 
   const data = searchResults ? searchResults : pokemons;
 
